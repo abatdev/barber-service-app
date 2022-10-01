e4395b232b40024626610c164ed9cc2cfb52ebc4
abatdev <abigailbatfreelance@outlook.com>
2022-10-01 16:33:37 -0700

    finish homepage frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,23 @@ import barberLogo from '../barber_logo.png'
 
 const Home = () =>{
   return (
-    <div>
-        <h1>HI</h1>
-        <img src={barberLogo} width="100" height="50" alt="Barber logo"></img>
+    <div className='row'>
 
+        <div className='container'>
+          <div className='child center'>
+            <img src={barberLogo} width="400" height="400" alt="Barber logo"></img>
+          </div>
+
+
+
+          <div className='child'>
+          <h2>Stay blessed by receiving<br></br> 
+          barber services through <br></br>
+          Blessed by Adean</h2>
+          </div>
+        </div>
+
+        <div id="startBtn"><button>Get Started</button></div>
     </div>
   )
 }
